c4a24666ecb9828928e5796e06e380fbd74a4099
Nadusha <nadusha230275@gmail.com>
2022-07-07 21:26:25 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// найти скмму чисел от 1 до а
+Console.WriteLine("введите число ");
+int a = int.Parse(Console.ReadLine());
+int p=0;
+for (int i=1; i<=a; i++)
+{
+    p=p+i;
+}
+    Console.WriteLine($"сумма чисел равна {p}");
+
